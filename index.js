23c120271c92791115b0514a55b7c9621e5c510d
Pranati Pradhan <pranati.pradhan@ingka.ikea.com>
2024-09-19 15:24:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const cors = require('cors');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const app = express();
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
+
+// Define User model
+class User extends Model {}
+User.init({
+    name: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN,
+}, { sequelize, modelName: 'user' });
+
+// Sync models with database
+sequelize.sync();
+
+const users = [
+    { name: "Ludwig Goransson",  isAdmin: false},
+    { name: "Johan Finndahl", isAdmin: false },
+    { name: "Marcus Nyh", isAdmin: true },
+    { name: "Pranati Pradhan", isAdmin: true },
+    { name: "Amar V", isAdmin: true  },
+    { name: "Saliya S", isAdmin: false  }
+];
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('react-frontend/dist'));
+
+app.get("/", (req, res) => {
+    res.json({ data: 'Hello Ikea!' });
+});
+
+app.get('/api/seeds', async (req, res) => {
+    users.forEach(user => User.create(user));
+    res.json(users);
+});
+
+app.get('/api/users', async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
+});
+
+app.get("/api/users/:id", async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    res.json(user);
+});
+
+app.post('/api/users', async (req, res) => {
+    const user = await User.create(req.body);
+    res.json(user);
+});
+
+app.put("/api/users/:id", async (req, res) => {
+    const { name, isAdmin } = req.body;
+
+    const user = await User.findByPk(req.params.id);
+    await user.update({ name, isAdmin });
+    await user.save();
+    res.json(user);
+});
+
+app.delete('/api/users/:id', async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    res.json({data: `The user with id of ${req.params.id} is removed.`});
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, async () => {
+    console.log('Server started at ${port}');
+});
---
+++ react-frontend-pranati/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const hostUrl = import.meta.env.PROD
+    ? window.location.href
+    : "http://localhost:8080/";
+
+const fetchUsers = async () => {
+  const response = await fetch(`${hostUrl}api/users`);
+  const usersToJson = await response.json();
+  console.log(usersToJson)
+  setUsers(usersToJson);
+};
+useEffect(() => {
+  fetchUsers();
+}, []);
+
+return (
+  <>
+    <h1>Users</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Is Admin</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.isAdmin}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </>
+);
+}
+
+export default App
